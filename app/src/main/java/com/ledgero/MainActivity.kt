c4bfd2e1e0de812ffe26626a848b2ff8f80db3bb
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-03-09 00:23:25 +0500

    added bottommnac bar in mainActivity

--- app/src/main/java/com/ledgero/MainActivity.kt
+++ app/src/main/java/com/ledgero/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ledgero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        TODO: change this line when dark mode needed in the activity
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //disabling dark mode from this activity
+
     }
 }
