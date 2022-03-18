6d598ebf52c6be965a1399204e8a625cb2def440
rana hamza <73963657+ranahamza4@users.noreply.github.com>
2022-03-18 17:51:03 +0500

    Commit Before Push - Refactor

--- app/src/main/java/com/ledgero/fragments/MoreFragment.kt
+++ app/src/main/java/com/ledgero/fragments/MoreFragment.kt
@@ -1,11 +1,21 @@
 package com.ledgero.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ledgero.LoginActivity
+import com.ledgero.MainActivity
 import com.ledgero.R
+import kotlinx.android.synthetic.main.fragment_more.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,10 +34,9 @@ class MoreFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
+
     }
 
     override fun onCreateView(
@@ -35,26 +44,24 @@ class MoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+
+        var view = inflater.inflate(R.layout.fragment_more, container, false)
+        var log = view.findViewById<Button>(R.id.logout)
+        Log.d("more", "onCreateView: skskdsdksignout ")
+
+        log.setOnClickListener(){
+
+            Log.d("more", "onCreateView: signout ")
+            var auth: FirebaseAuth = Firebase.auth
+            auth.signOut()
+            startActivity(Intent(view.context,LoginActivity::class.java))
+            var ac= view.context as Activity
+            ac.finish()
+
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MoreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MoreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }
