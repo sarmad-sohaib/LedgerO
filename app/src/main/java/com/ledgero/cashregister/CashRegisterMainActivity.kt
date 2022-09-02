66b646554cab41ed9f38496fecadc065e157d6fe
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-09-02 13:24:58 +0500

    Cash Register feature (innit)

---
+++ app/src/main/java/com/ledgero/cashregister/CashRegisterMainActivity.kt
@@ -0,0 +1,12 @@
+package com.ledgero.cashregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ledgero.R
+
+class CashRegisterMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cash_register_main)
+    }
+}
