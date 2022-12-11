aa99dde9c71a23148f95ec7d18cfd889e53c1e30
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-12-11 13:56:59 +0500

    Large Scale Refactor: Whole App UI is refactored for darkMode and RTL

--- app/src/main/java/com/ledgero/more/AppThemeDialogFragment.kt
+++ app/src/main/java/com/ledgero/more/AppThemeDialogFragment.kt
@@ -1,28 +1,22 @@
 package com.ledgero.more
 
 import android.app.Dialog
-import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.ledgero.more.ui.MoreViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 private const val TAG = "AppThemeDialog"
 
 @AndroidEntryPoint
-class AppThemeDialogFragment() : DialogFragment() {
+class AppThemeDialogFragment : DialogFragment() {
 
     private val job = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
