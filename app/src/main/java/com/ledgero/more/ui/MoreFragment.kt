e542e2e00c6605787d2362d385d5026c87cd62b9
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-12-12 21:56:24 +0500

    Bugfix: RTl, Language, Transalation

--- app/src/main/java/com/ledgero/more/ui/MoreFragment.kt
+++ app/src/main/java/com/ledgero/more/ui/MoreFragment.kt
@@ -1,21 +1,25 @@
 package com.ledgero.more.ui
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ledgero.LoginActivity
+import com.ledgero.R
 import com.ledgero.databinding.FragmentMoreBinding
 import com.ledgero.more.AppThemeDialogFragment
 import com.ledgero.more.LanguageModelBottomSheet
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import java.util.*
 
 private const val TAG = "MoreFragment"
@@ -24,8 +28,8 @@ private const val TAG = "MoreFragment"
 class MoreFragment : Fragment() {
 
     private lateinit var binding: FragmentMoreBinding
+    private val moreViewModel: MoreViewModel by viewModels()
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +37,24 @@ class MoreFragment : Fragment() {
 
         binding = FragmentMoreBinding.inflate(inflater)
         val local = Locale.getDefault()
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                moreViewModel.prefFlow.collect { key ->
+                    when (key) {
+                        0 -> {
+                            binding.textViewCurrentTheme.text = getString(R.string.system_default)
+                        }
+                        1 -> {
+                            binding.textViewCurrentTheme.text = getString(R.string.dark_mode)
+                        }
+                        2 -> {
+                            binding.textViewCurrentTheme.text = getString(R.string.light_mode)
+                        }
+                    }
+                }
+            }
+        }
 
         binding.apply {
 
